e7c830e5c40405edbc82191e5528048db0beb6ad
blazhko22 <blazhko22@gmail.com>
2021-11-14 17:35:51 +0200

    дз-7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const imagesMarkup = makeImageCard(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function makeImageCard(imgs) {
+  return imgs
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>
+`;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", ongalleryContainerClick);
+
+function ongalleryContainerClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+}
+let lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
